4916a9216104be43abf92fe66a93f90aae02a1f6
zpxel <zpixeljy@gmail.com>
2025-01-22 13:58:49 +0800

    Update run.sh
    New update for this shell to run dependencies

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 pkg update && pkg upgrade
 pkg install python
 pkg install openssh
-cp cave.py /data/data/com.termux/files/home
+pip uninstall requests urllib3 six
+pip install requests urllib3 six
+pkg install clang libffi openssl
+cp maavar.py /data/data/com.termux/files/home
 cd
 cd
-python3 cave.py
+python3 maavar.py
