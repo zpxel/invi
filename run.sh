ea6d6942a02a2599909b53665b5843ad291a2c5d
zpxel <zpixeljy@gmail.com>
2025-01-22 15:54:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 pkg update && pkg upgrade
+termux-setup-storage
 pkg install python
 pkg install openssh
 pip uninstall requests urllib3 six
 pip install requests urllib3 six
 pkg install clang libffi openssl
-mv maavar.py /data/data/com.termux/files/home
-termux-setup-storage
+cp maavar.py /data/data/com.termux/files/home
 cd
 cd
 python3 maavar.py
