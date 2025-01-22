47e76ae21a25a57a073fcc65f8ab4c606e944579
zpxel <zpixeljy@gmail.com>
2025-01-22 15:41:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ pkg install openssh
 pip uninstall requests urllib3 six
 pip install requests urllib3 six
 pkg install clang libffi openssl
-mv maavar.py /data/data/com.termux/files/home
+cp maavar.py /data/data/com.termux/files/home
 cd
 cd
-python3 maavar.py
